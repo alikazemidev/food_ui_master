2a654777dccb33b0946c9de063356ab9d95e6ca5
alikz <ali6316k@gmail.com>
2022-06-29 19:10:03 +0430

    complete restaurant screen

--- lib/screens/restaurant_screen.dart
+++ lib/screens/restaurant_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:food_master_ui/models/food.dart';
 import 'package:food_master_ui/models/restaurant.dart';
 import 'package:food_master_ui/widgets/rating_starts.dart';
 
@@ -114,6 +115,106 @@ class _RestaurantScreenState extends State<RestaurantScreen> {
               ),
             ],
           ),
+          SizedBox(height: 10),
+          Center(
+            child: Text(
+              'Menu',
+              style: TextStyle(
+                fontSize: 22,
+                letterSpacing: 1.2,
+                fontWeight: FontWeight.w600,
+              ),
+            ),
+          ),
+          SizedBox(height: 10),
+          Expanded(
+            child: GridView.count(
+              mainAxisSpacing: 20,
+              crossAxisSpacing: 20,
+              padding: EdgeInsets.all(10),
+              crossAxisCount: 2,
+              children: List.generate(widget.restaurant.menu.length, (index) {
+                Food food = widget.restaurant.menu[index];
+                return _buildMenuItem(food);
+              }),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  _buildMenuItem(Food menuItem) {
+    return Center(
+      child: Stack(
+        children: [
+          Container(
+            height: 175,
+            width: 175,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(15),
+              image: DecorationImage(
+                fit: BoxFit.cover,
+                image: AssetImage(menuItem.imageUrl),
+              ),
+            ),
+            foregroundDecoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(15),
+              gradient: LinearGradient(
+                begin: Alignment.topRight,
+                end: Alignment.bottomLeft,
+                colors: [
+                  Colors.black.withOpacity(0.3),
+                  Colors.black87.withOpacity(0.3),
+                  Colors.black54.withOpacity(0.3),
+                  Colors.black38.withOpacity(0.3),
+                ],
+                stops: [.1, .5, .7, .9],
+              ),
+            ),
+          ),
+          Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  menuItem.name,
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold,
+                    letterSpacing: 1.2,
+                  ),
+                ),
+                Text(
+                  '\$${menuItem.price.toString()}',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold,
+                    letterSpacing: 1.2,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Positioned(
+            bottom: 10,
+            right: 10,
+            child: Container(
+              width: 48,
+              height: 48,
+              decoration: BoxDecoration(
+                color: Theme.of(context).colorScheme.primary,
+                borderRadius: BorderRadius.circular(1000),
+              ),
+              child: Icon(
+                Icons.add,
+                size: 30,
+                color: Colors.white,
+              ),
+            ),
+          )
         ],
       ),
     );
